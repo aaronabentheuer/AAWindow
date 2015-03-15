6305250ba0dd3c8b606c031eed2f823ee50db528
Aaron Abentheuer <aaron.abenth@icloud.com>
2015-03-15 18:01:38 +0100

    Initial Commit

---
+++ AAWindowExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AAWindowExample
+//
+//  Created by Julian Abentheuer on 15.03.15.
+//  Copyright (c) 2015 overview+DETAIL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
